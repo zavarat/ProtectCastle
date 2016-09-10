f0fe2e34aa0448f3093a3e670be1804011d2cf33
장진우 <zzangjjw4406@gmail.com>
2016-09-10 12:30:18 +0900

    # 미사일 이펙트 및 공격 컨트롤러 추가 및 테스트
    1. 미사일 이펙트 추가.
    2. 공격 컨트롤러 스크립트 생성 및 테스트.

---
+++ Assets/KojeomStudio/Scripts/AttackController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AttackController : MonoBehaviour {
+    [SerializeField]
+    private Transform attacker;
+    [SerializeField]
+    private GameObject missilePrefab;
+    private Queue<GameObject> missiles = new Queue<GameObject>();
+    private GameObject target;
+    private Transform parentTrans;
+    private IEnumerator attackProcess;
+
+    public int maxQuantity = 5;
+    [Range(1.0f, 5.0f)]
+    public float attackIntervalTime = 1.0f;
+
+	public void Init () {
+        attackProcess = AttackProcess();
+        parentTrans = GameObject.FindGameObjectWithTag("missileBucket").transform;
+        target = GameObject.FindGameObjectWithTag("target");
+
+	    for(int idx = 0; idx < maxQuantity; idx++)
+        {
+            GameObject mis = Instantiate(missilePrefab, attacker.position,
+                new Quaternion(0, 0, 0, 0)) as GameObject;
+            mis.transform.parent = parentTrans;
+            EffectSettings settings = mis.GetComponent<EffectSettings>();
+            settings.Target = target;
+            settings.EffectDeactivated += (n, e) =>
+            {
+                mis.SetActive(false);
+            };
+
+            mis.SetActive(false);
+            missiles.Enqueue(mis);
+        }
+    }
+	
+    public void StartAttack()
+    {
+        StartCoroutine(attackProcess);
+    }
+
+    public void StopAttack()
+    {
+        StopCoroutine(attackProcess);
+    }
+
+    private IEnumerator AttackProcess()
+    {
+        while (true)
+        {
+            GameObject mis = missiles.Dequeue();
+            if((mis != null) && (!mis.activeSelf))
+            {
+                mis.transform.position = attacker.transform.position;
+                mis.SetActive(true);
+                missiles.Enqueue(mis);
+            }
+            yield return new WaitForSeconds(attackIntervalTime);
+        }
+    }
+}
---
+++ Assets/KojeomStudio/Scripts/EnemyAI/BT_base.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public abstract class Node
+{
+    public abstract bool Invoke();
+}
+
+public class CompositeNode : Node
+{
+    public override bool Invoke()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddChild(Node node)
+    {
+        childrens.Push(node);
+    }
+
+    public Stack<Node> GetChildrens()
+    {
+        return childrens;
+    }
+    private Stack<Node> childrens = new Stack<Node>();
+}
+
+public class Selector : CompositeNode
+{
+    public override bool Invoke()
+    {
+        foreach (var node in GetChildrens())
+        {
+            if (node.Invoke())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class Sequence : CompositeNode
+{
+    public override bool Invoke()
+    {
+        foreach (var node in GetChildrens())
+        {
+            if (!node.Invoke())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class MoveForTarget : Node
+{
+    public NavMeshAgent agent
+    {
+        set { _agent = value; }
+    }
+    private NavMeshAgent _agent;
+    public Transform target
+    {
+        set { _target = value; }
+    }
+    private Transform _target;
+    public override bool Invoke()
+    {
+        Debug.Log("Move for Target!");
+        _agent.destination = _target.position;
+        return true;
+    }
+    
+}
+
+public class BT_base : MonoBehaviour {
+
+    private Sequence root = new Sequence();
+    private Selector selector = new Selector();
+    
+    [SerializeField]
+    private NavMeshAgent enemyAgent;
+    [SerializeField]
+    private Transform target;
+
+    private IEnumerator behaviorProcess;
+
+    // test start func
+	void Start () {
+        root.AddChild(selector);
+
+        behaviorProcess = BehaviorProcess();
+	}
+    public void StartBT()
+    {
+        StartCoroutine(behaviorProcess);
+    }
+    public void StopBT()
+    {
+        StopCoroutine(behaviorProcess);
+    }
+
+    private IEnumerator BehaviorProcess()
+    {
+        while (!root.Invoke())
+        {
+            Debug.Log("======================");
+            yield return new WaitForSeconds(1.0f);
+        }
+        Debug.Log("behavior process exit");
+    }
+	
+	
+}
